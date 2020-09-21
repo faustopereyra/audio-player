e8aea327d3349c2de8ca7ac2056b4f8ab3f2ce36
faustopereyra <pfausto@live.com.ar>
2020-09-21 00:03:34 -0300

    set css variables, clean app, add book and library components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Library from "library/library.component.jsx"
+import Book from "book/book.component.jsx"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Library />
+      <Book />
     </div>
   );
 }
